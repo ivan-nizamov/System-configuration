864f45e6bc29d056c9e8d639038d746adf45db04
navi <navi@laptop>
2025-09-18 11:20:10 +0300

    removed legacy config

--- home/navi/user-base.nix
+++ home/navi/user-base.nix
@@ -6,7 +6,7 @@
     # Import desktop configuration for all machines
     imports = [ 
         ./wayland-desktop.nix
-        ./programs/emacs/config/emacsV2/tangle.nix
+        ./programs/emacs/config/tangle.nix
         ./programs/music/mpd.nix
         ./programs/music/rmpc.nix
         ./programs/vscode
