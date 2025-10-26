1ca71c0edb54a9d126c786440f5000cecffc2724
navi <navi@laptop>
2025-10-26 09:14:57 +0200

    Add desktop-windows Syncthing device

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -32,13 +32,17 @@
       # Your devices (IDs are NOT secret—paste from each peer’s GUI)
       devices = {
         "phone" = { id = "2WGUA6E-OJLUMYK-7NJLC4C-42JON6A-FOLZZQX-VT4JEGC-AA3CGJ7-BPNFZQX"; addresses = [ "dynamic" ]; };
+        "desktop-windows" = {
+          id = "M6Z45DS-BSYJV34-Y4RVGL5-7LTLBIU-KL35BCR-E5YTV6C-2KXIHEE-IL2I4AH";
+          addresses = [ "dynamic" ];
+        };
       };
 
       # Folders: names are arbitrary keys; path is local
       folders = {
         "Common\ ground" = {
           path = "/home/navi/Common\ ground/";
-          devices = [ "phone" ];
+          devices = [ "phone" "desktop-windows" ];
           type = "sendreceive";      # or "receiveonly"
           fsWatcherEnabled = true;   # inotify; instantish sync
           ignorePerms = true;        # cross-OS friendliness
